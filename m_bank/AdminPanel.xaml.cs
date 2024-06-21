5252b1c8749b752ca59025737093471abc7182fe
Mukhethwa.Magadani <masikngl@gmail.com>
2024-06-21 11:00:02 +0000

    Updates

--- m_bank/AdminPanel.xaml.cs
+++ m_bank/AdminPanel.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +21,13 @@ namespace m_bank
     /// </summary>
     public partial class AdminPanel : Window
     {
+        SqlConnection con;
         public AdminPanel()
         {
             InitializeComponent();
+            string connectionString = "  Data Source = labG9AEB3\\SQLEXPRESS; Initial Catalog = Bank; Integrated Security = True; Encrypt = True; Trust Server Certificate = True";
+            con = new SqlConnection(connectionString);
+            con.Open();
         }
 
         private void btnAdminPanelSignOut_Click(object sender, RoutedEventArgs e)
